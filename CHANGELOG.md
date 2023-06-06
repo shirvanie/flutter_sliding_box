e2763d63f3a26c7736d5bcdb29b3536e3defa388
Hamed Shirvani <ihamedshirvani@gmail.com>
2023-06-07 02:54:31 +0330

    Initial Release

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+
+## [1.0.0] - 29 May, 2023
+- Initial Release
---
+++ lib/flutter_sliding_box.dart
@@ -0,0 +1,9 @@
+/*
+© 2023, Hamed Shirvani. All rights reserved.
+Im Hamed Shirvani, i build and defines this plugin in 5/29/23
+LICENSE: https://github.com/shirvanie/flutter_sliding_box/blob/master/LICENSE
+*/
+
+library flutter_sliding_box;
+
+export 'src/flutter_sliding_box.dart';
